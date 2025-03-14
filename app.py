fc4221d112517b8cf58e959bf1ce1f08956f3a70
bruna0204 <bruna02042008@gmail.com>
2025-03-14 16:25:50 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+@app.route('/<name>') #aparece no nevegador /qualquer coisa
+def hello(name):
+    return 'Hello' + name
+
+
+@app.route('/soma/<number1>+<number2>')
+def soma(number1, number2):
+    try:
+        number1 = int(number1)
+        number2 = int(number2)
+        return str(number1 + number2)
+    except ValueError:
+        return 'Só numeros inteiros'
+
+@app.route('/subtracao/<number1>-<number2>')
+def subtract(number1, number2):
+    try:
+        number1 = int(number1)
+        number2 = int(number2)
+        return str(number1 - number2)
+    except ValueError:
+        return 'Só numeros inteiros'
+
+@app.route('/multiplicacao/<number1>*<number2>')
+def multiply(number1, number2):
+    try:
+        number1 = int(number1)
+        number2 = int(number2)
+        return str(number1 * number2)
+    except ValueError:
+        return 'Somente numeros inteiros'
+
+@app.route('/divisao/<number1>/<number2>')
+def divisao(number1, number2):
+        try:
+            number1 = int(number1)
+            number2 = int(number2)
+            if number1 / number2 == 0:
+                return str (number1 / number2)
+            else:
+                return 'O numero precisa ser maior que zero'
+
+        except ValueError:
+            print("Digite apenas numeros inteiros")
+
+
+@app.route('/par_impar/<number1>/<number2>')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True) #presisa desabilitar ele quando subir pra internet
